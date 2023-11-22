3fe84ecad5499d68a7c6c66a97fd33fc3c5269bc
MAC ITADVIP <macitadvip@192.168.1.8>
2023-11-22 06:06:35 -0800

    push components

--- lib/news/presentation/screens/news_screen.dart
+++ lib/news/presentation/screens/news_screen.dart
@@ -10,7 +10,7 @@ class NewsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => sl<NewsCubit>(),
+      create: (context) => sl<NewsCubit>()..getNews(),
       child: Scaffold(
         appBar: AppBar(
           title: const Text(
